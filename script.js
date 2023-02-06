8f3d3842230fdd4616596e8186aa6ee5030a9447
ElTitovski <elena.titovski@gmail.com>
2023-02-06 21:05:45 +0300

    update nav + contacts

---
+++ script.js
@@ -0,0 +1,15 @@
+/* window.onscroll = function() {myFunction()};
+
+
+var contener__header = document.getElementsByClassName("contener__header");
+
+var sticky = contener__header.offsetTop;
+
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    contener__header.classList.add("sticky")
+  } else {
+    contener__header.classList.remove("sticky");
+  }
+} */
