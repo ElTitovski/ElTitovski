f16091d6453a08102161e698bd2b2e348d8e72f1
ElTitovski <elena.titovski@gmail.com>
2023-02-07 21:31:46 +0300

    add html servises

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function myFunction() {
     contener__header.classList.remove("sticky");
   }
 } */
+
+var link = document.querySelector('.main__link', '.portfolio__link', '.services__link', 'contacts__link' )
+
+link.addEventListener('click', function(evt){
+  evt.preventDefault();
+  console.log('Клик по ссылке home');
+
+});
+
